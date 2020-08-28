20ffd833b76bfdad48c12587451dfc5d14a19d98
jin-jeffrey <49298246+jin-jeffrey@users.noreply.github.com>
2020-08-27 22:51:47 -0400

    added Introduction component

---
+++ personalwebsite/src/components/Introduction.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Introduction extends React.Component {
+    render(){
+        return(
+            <container>
+                <div>
+                    Hello, my name is Jeffrey Jin. Welcome to my page! Here's a little about me:
+                </div>
+            </container>
+        );
+    }
+};
+
+export default Introduction;
