91183802d08def64df62fe35f8c272aab47b985b
jin-jeffrey <49298246+jin-jeffrey@users.noreply.github.com>
2020-08-31 16:59:31 -0400

    updated project styling

--- personalwebsite/src/components/Card/Card.js
+++ personalwebsite/src/components/Card/Card.js
@@ -4,11 +4,11 @@ import './Card.css'
 const Card = (props) => {
     return (
         // vertical cards
-        <div className = "card text-center" style={{width:"20vw", height:"40vh"}}>
-            <div className = "overflow">
+        <div className = "card text-center mx-5 img-hover">
+            <div className = "overflow-hidden">
                 <a href={props.link} target="_blank" rel="noopener noreferrer"><img className="card-img-top cardimg" src={props.imgsrc} alt={props.alt}/></a>
             </div>
-            <div className="card-body text-dark bodytext">
+            <div className="card-body overflow-auto">
                 <h4 className="card-title">{props.name}</h4>
                 <p className="card-text text-secondary">{props.description}</p>
             </div>
@@ -16,20 +16,6 @@ const Card = (props) => {
                 <small className="text-muted">{props.tech}</small>
             </div>
         </div>
-        // <div className="mb-3 card-project">
-        //     <div className="row">
-        //         <div className="col-md-6">
-        //             <img src={props.imgsrc} className="card-img" alt={props.alt} />
-        //         </div>
-        //         <div className="col-md-6">
-        //             <div className="descriptions">
-        //                 <h5 className="">{props.name}</h5>
-        //                 <p className="">{props.description}</p>
-        //                 <p className=""><small className="text-muted tech">{props.tech}</small></p>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
     );
 }
 
--- personalwebsite/src/components/Projects/Projects.js
+++ personalwebsite/src/components/Projects/Projects.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Card from '../Card/Card.js'
 import INVM from './media/INVM.png';
-import google from './media/google.jpg'
 import './Projects.css';
 
 
@@ -10,32 +9,16 @@ class Projects extends React.Component {
         return(
             <container className="projects my-5">
                 <div className="projectTitle mb-5">PROJECTS</div>
-                <div className="container-fluid d-flex justify-content-center">
-                    <div className="row">
-                        <div className="col proj-item"><Card imgsrc={INVM} alt="INVM" name="INVM"
-                        link="https://www.invm.dev"
-                        tech="HTML5, CSS3, Python, Django, Bootstrap"
-                        description="Inventory Manager Project"/></div>
-                        <div className="col"><Card imgsrc={google} alt="googleactivity" name="Google Activity Generator"
-                        link="https://github.com/jin-jeffrey/gmail-activity"
-                        tech="Python, Selenium"
-                        description="Generate google activity on"
-                        /></div>
-                        <div className="col"><Card imgsrc={INVM} alt="hello"/></div>
-                    </div>
-                    <div className="row">
-                        <div className="col proj-item"><Card imgsrc={INVM} alt="INVM" name="INVM"
-                        link="https://www.invm.dev"
-                        tech="HTML5, CSS3, Python, Django, Bootstrap"
-                        description="Inventory Manager Project"/></div>
-                        <div className="col"><Card imgsrc={google} alt="googleactivity" name="Google Activity Generator"
-                        link="https://github.com/jin-jeffrey/gmail-activity"
-                        tech="Python, Selenium"
-                        description="Generate google activity on"
-                        /></div>
-                        <div className="col"><Card imgsrc={INVM} alt="hello"/></div>
-                    </div>
-                </div>     
+                <div className="proj-group">
+                    <div className="proj-item"><Card imgsrc={INVM} alt="INVM" name="INVM: Inventory Manager"
+                    link="https://www.invm.dev"
+                    tech="HTML5, CSS3, Python, Django, Bootstrap"
+                    description="A webapp designed to help resellers track inventory, sales, and profits. Users can add items, specify item details, and log transactions. "/></div>
+                    <div className="proj-item"><Card imgsrc="" alt="Google" name="Google Activity Generator"
+                    link="https://github.com/jin-jeffrey/gmail-activity"
+                    tech="Python, Selenium"
+                    description="A python project used to fully automate activity on a gmail account. Functionality include signing in, searching Google for articles and images, and watching videos on Youtube."/></div>
+                </div>
             </container>
         );
     }
