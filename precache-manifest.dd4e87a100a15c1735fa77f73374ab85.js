863568aaf8ed7737489b4f4849ee8a08c81fa55c
jin-jeffrey <49298246+jin-jeffrey@users.noreply.github.com>
2020-09-01 18:15:36 -0400

    Updates

---
+++ precache-manifest.dd4e87a100a15c1735fa77f73374ab85.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7102c733cf18266c929e56ac30ded123",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f88c061bea0ef55bb6c4",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "beb6808a1eeb2965b72e",
+    "url": "/static/css/main.79a5b192.chunk.css"
+  },
+  {
+    "revision": "f88c061bea0ef55bb6c4",
+    "url": "/static/js/2.8ced6740.chunk.js"
+  },
+  {
+    "revision": "2927c17f155caba9c734acdc50e6f829",
+    "url": "/static/js/2.8ced6740.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beb6808a1eeb2965b72e",
+    "url": "/static/js/main.e0db191f.chunk.js"
+  },
+  {
+    "revision": "4a622ea621e71266c231",
+    "url": "/static/js/runtime-main.2b353301.js"
+  },
+  {
+    "revision": "62e0444e8c6f1127646eda164095f78f",
+    "url": "/static/media/INVM.62e0444e.png"
+  },
+  {
+    "revision": "96bcb06669b7e8a10d148b7f1f050ee3",
+    "url": "/static/media/bg-gen.96bcb066.png"
+  },
+  {
+    "revision": "920850c57965fa5a0c892e3ea0237738",
+    "url": "/static/media/code.920850c5.png"
+  },
+  {
+    "revision": "4e66e46d7e1158ea07c89a310a139592",
+    "url": "/static/media/github.4e66e46d.png"
+  },
+  {
+    "revision": "99eb72029cc2e98fdf0b61e368ce4d32",
+    "url": "/static/media/google.99eb7202.png"
+  },
+  {
+    "revision": "2b58af2f91e4bfbdd01c8348d90a57cf",
+    "url": "/static/media/linkedin.2b58af2f.png"
+  },
+  {
+    "revision": "9885e19a9098c918e5935b5937d36488",
+    "url": "/static/media/resume.9885e19a.pdf"
+  },
+  {
+    "revision": "99133fe49844f91bf3877e6a0137f204",
+    "url": "/static/media/resume.99133fe4.png"
+  }
+]);
